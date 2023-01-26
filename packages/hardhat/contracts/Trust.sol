4d1bbd97eeb7942d79f9746def24d8ce5efb7b85
BrutKode <usefulvloging@gmail.com>
2023-01-26 17:50:00 +0530

    Deploy Contracts

--- packages/hardhat/contracts/Trust.sol
+++ packages/hardhat/contracts/Trust.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Trust {
 
     uint256 pot;
-    address owner;
+    address public owner;
     IERC20 token;
 
     event employ(address developer, uint amount);
